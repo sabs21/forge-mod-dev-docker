f4abc924ccadc84e40faef5dba4876dee91017bf
Nick Sabia <nms00010101@outlook.com>
2023-11-14 13:09:55 -0500

    added batch for copying compiled mod from docker volume to local minecraft client mods folder

---
+++ copy_mod_to_client.bat
@@ -0,0 +1,73 @@
+@echo off
+set dockerpath="%programfiles%\Docker\Docker\Docker Desktop.exe"
+set attempts=20
+
+echo Checking if Docker is currently running...
+:openDocker
+docker info | find /i "Server Version">NUL
+if %errorlevel% equ 0 (
+	goto :dockerSuccess
+) 
+if %errorlevel% equ 1 (
+	echo Attempts left: %attempts% 
+	if %attempts% equ 20 (
+		start "" %dockerpath% && echo Starting Docker...
+	)
+	set /a attempts-=1 
+	timeout /t 2 /nobreak > NUL 
+	goto :openDocker
+)
+echo Unable to start Docker. Exiting... 
+pause 
+exit
+
+:dockerSuccess
+echo Docker is running.
+pause
+
+set containerName="mc-dev"
+set serviceName="dev"
+
+echo Checking if development containers are running...
+docker ps --filter "name=%containerName%" --filter "status=running" | find /i %containerName%>NUL
+if %errorlevel% equ 0 (
+	goto :containerSuccess
+) 
+if %errorlevel% equ 1 (
+	docker ps --filter "name=%containerName%" | find /i %containerName%>NUL
+	if %errorlevel% equ 0 (
+		goto :startContainer
+	) 
+	if %errorlevel% equ 1 (
+		goto :createContainer
+	)
+)
+:createContainer
+echo Creating development container...
+docker compose create --build %serviceName%
+
+:startContainer
+echo Starting development container...
+docker compose start %serviceName%
+
+:containerSuccess
+echo Development container %containerName% is running.
+pause
+
+set volumeName=mod-build
+set minecraftModsPath="%userprofile%\AppData\Roaming\.minecraft\mods"
+
+echo Copying mod files to client minecraft mods folder...
+:: https://stackoverflow.com/questions/35406213/how-to-copy-data-from-docker-volume-to-host
+cd %minecraftModsPath%
+::docker ps -aqf "name=%containerName%"
+for /f %%i in ('docker run -d -v %volumeName%:/%volumeName% busybox true') do (
+	docker cp %%i:/%volumeName% ./
+	docker rm %%i
+)
+cd %volumeName%
+for /r "./" %%i in (*) do move /Y %%i ..
+cd ..
+rmdir %volumeName%
+pause
+exit
